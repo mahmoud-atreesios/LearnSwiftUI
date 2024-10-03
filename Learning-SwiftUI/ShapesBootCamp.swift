7dff28a6b5fb001da81306f8820708f396c4ec77
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-10-03 04:38:15 +0300

    ShapesBootCamp

--- Learning-SwiftUI/ShapesBootCamp.swift
+++ Learning-SwiftUI/ShapesBootCamp.swift
@@ -7,4 +7,25 @@
 
 import SwiftUI
 
+struct ShapesBootCamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+        //Rectangle()
+        //Capsule(style: .circular)
+        //Ellipse()
+        //Circle()
+            //.fill(Color.yellow)
+            //.foregroundColor(.cyan)
+            //.stroke(Color.red,lineWidth: 20)
+            //.stroke(Color.cyan, style: StrokeStyle(lineWidth: 10, lineCap: .round, dash: [50]))
+            //.trim(from: 0.2, to: 1)
+            .stroke(Color.cyan, lineWidth: 3)
+            .frame(width: 370, height: 70)
+    }
+}
 
+struct ShapesBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesBootCamp()
+    }
+}
