b04ae52a7b2640f31703a950ee3e893b604e2c28
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-10-11 05:34:28 +0300

    ScrollViewBootCamp

---
+++ Learning-SwiftUI/BootCamps/ScrollViewBootCamp.swift
@@ -0,0 +1,60 @@
+//
+//  ScrollViewBootCamp.swift
+//  Learning-SwiftUI
+//
+//  Created by Mahmoud Mohamed Atrees on 11/10/2024.
+//
+
+import SwiftUI
+
+struct ScrollViewBootCamp: View {
+    var body: some View {
+        ScrollView {
+            ForEach(0..<30) { index in
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .shadow(radius: 5)
+                    .padding(.horizontal)
+                    .frame(height: 150)
+                    .overlay {
+                        HStack(alignment: .top, spacing: 2) {
+                            Image("EtihadStadium")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedCorner(radius: 20, corners: [.topLeft, .bottomLeft]))
+                                .padding(.horizontal)
+                            StacksBootCamp()
+                                .padding(.vertical)
+                            Spacer()
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct ScrollViewBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewBootCamp()
+    }
+}
+
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
--- Learning-SwiftUI/BootCamps/StacksBootCamp.swift
+++ Learning-SwiftUI/BootCamps/StacksBootCamp.swift
@@ -9,15 +9,45 @@ import SwiftUI
 
 struct StacksBootCamp: View {
     var body: some View {
-        VStack(alignment: .center, spacing: 5) {
-            Text("5")
-                .font(.title)
-                .underline()
-                .bold()
-            Text("items in your carts")
-                .foregroundColor(.gray)
-                .font(.caption)
+        VStack(alignment: .leading,spacing: 8){
+                Text("Arsenal vs ManCity")
+                    .font(.callout)
+            HStack{
+                Image(systemName: "clock.circle")
+                    .resizable()
+                    .foregroundColor(Color(.sRGBLinear, red: 0.043137254901960784, green: 0.09803921568627451, blue: 0.17254901960784313))
+                    .frame(width: 25, height: 25)
+                Text("1:30")
+                    .font(.caption)
+            }
+            HStack{
+                Image(systemName: "calendar.circle")
+                    .resizable()
+                    .foregroundColor(Color(.sRGBLinear, red: 0.043137254901960784, green: 0.09803921568627451, blue: 0.17254901960784313))
+                    .frame(width: 25, height: 25)
+                Text("2024-12-12")
+                    .font(.caption)
+            }
+            HStack{
+                Image(systemName: "location.circle")
+                    .resizable()
+                    .foregroundColor(Color(.sRGBLinear, red: 0.043137254901960784, green: 0.09803921568627451, blue: 0.17254901960784313))
+                    .frame(width: 25, height: 25)
+                Text("Cairo Stadium")
+                    .font(.caption)
+            }
         }
+        .bold()
+        
+//        VStack(alignment: .center, spacing: 5) {
+//            Text("5")
+//                .font(.title)
+//                .underline()
+//                .bold()
+//            Text("items in your carts")
+//                .foregroundColor(.gray)
+//                .font(.caption)
+//        }
     }
 }
 
