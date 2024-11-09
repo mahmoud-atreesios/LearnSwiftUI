ec01d072c9c7f93c5a926505781839f0350d87cc
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-11-09 16:19:44 +0200

    finish onboarding screen

--- Learning-SwiftUI/BootCamps/OnboardingScreen.swift
+++ Learning-SwiftUI/BootCamps/OnboardingScreen.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct OnboardingScreen: View {
     
     @State var selectedPage: Int = 0
-    @State var images = ["fourth","123","hhh","223"]
+    @State var images = ["fourth","223","hhh","123"]
     @State var titles = ["Explore Recipes","Utilize LeftOvers","Share Recipes","Meal Schedule"]
     @State var subTitles = ["We provide recipes based on what you have on hand","Start with ingredients that you have in kitchen","You can invite your partner to cook with you","Build a meal plan to meet nutrition needs"]
     
     
     var body: some View {
-        VStack {
+        ZStack {
             TabView(selection: $selectedPage) {
                 ForEach(0..<titles.count, id: \.self) { index in
                     OnboardingScreenShape(selectedPage: $selectedPage,
@@ -28,23 +28,22 @@ struct OnboardingScreen: View {
                 }
             }
             .tabViewStyle(.page)
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
             .ignoresSafeArea()
+            
         }
     }
 }
 
-//lll
-
-
 struct OnboardingScreenShape: View {
     
     @Binding var selectedPage: Int
-
+    
     var image: String
     var title: String
     var subTitle: String
     var pageIndex: Int
-        
+    
     var body: some View {
         ZStack {
             
@@ -60,8 +59,10 @@ struct OnboardingScreenShape: View {
                 .ignoresSafeArea()
             
             Button {
-                print("Skip Button pressed")
-                selectedPage = 0
+                withAnimation {
+                    print("Skip Button pressed")
+                    selectedPage = 0
+                }
             } label: {
                 HStack(spacing: 3) {
                     Text("Skip")
@@ -74,12 +75,10 @@ struct OnboardingScreenShape: View {
             
             VStack{
                 Spacer()
-                
                 Text(title)
                     .font(.largeTitle)
                     .opacity(selectedPage == pageIndex ? 1 : 0)
                     .animation(.easeOut, value: selectedPage)
-
                 
                 Text(subTitle)
                     .font(.title2)
@@ -87,13 +86,33 @@ struct OnboardingScreenShape: View {
                     .animation(.easeOut, value: selectedPage)
                     .padding(.vertical,1)
                     .padding(.horizontal)
-                    
-                
             }
             .frame(width: UIScreen.main.bounds.width)
             .bold()
             .foregroundColor(.white)
             .padding(.vertical, 100)
+            
+            if selectedPage == 3{
+                VStack {
+                    Spacer()
+                    Button {
+                        print("")
+                    } label: {
+                        Text("Get started")
+                            .frame(width: 230, height: 60)
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .bold()
+                            .background {
+                                RoundedRectangle(cornerRadius: 40)
+                                    .foregroundColor(Color(red: 255/255, green: 178/255, blue: 111/255))
+                            }
+                            .padding()
+                    }
+                    .opacity(selectedPage == pageIndex ? 1 : 0)
+                    .animation(.easeOut, value: selectedPage)
+                }
+            }
         }
     }
 }
